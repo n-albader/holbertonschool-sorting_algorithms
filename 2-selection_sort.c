1f07049491d2dae5ab202eb7c9311c492faac7e4
n-albader <noor.abdullah.albader@gmail.com>
2026-08-17 12:48:46 +0300

    add Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Sorts an array of integers using Selection sort
+ * @array: Array to sort
+ * @size: Number of elements in array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+	int temp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+
+			print_array(array, size);
+		}
+	}
+}
