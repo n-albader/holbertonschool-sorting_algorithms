4fdf13573aaa481f6cc55c50bd407744a3ae1a55
Lujain Alsultan <lujainabdulmohsen@hotmail.com>
2026-08-20 18:45:56 +0300

    Fix Betty style for Counting sort

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -2,7 +2,48 @@
 #include "sort.h"
 
 /**
- * counting_sort - Sorts an array using the Counting sort algorithm
+ * get_max - Finds the largest value in an array
+ * @array: Array to inspect
+ * @size: Size of the array
+ *
+ * Return: Largest value
+ */
+static int get_max(const int *array, size_t size)
+{
+	int max;
+	size_t i;
+
+	max = array[0];
+	for (i = 1; i < size; i++)
+		if (array[i] > max)
+			max = array[i];
+
+	return (max);
+}
+
+/**
+ * build_count - Builds the cumulative counting array
+ * @array: Array to count
+ * @size: Size of the array
+ * @count: Counting array
+ * @max: Largest value in array
+ */
+static void build_count(int *array, size_t size, int *count, int max)
+{
+	size_t i;
+
+	for (i = 0; i <= (size_t)max; i++)
+		count[i] = 0;
+
+	for (i = 0; i < size; i++)
+		count[array[i]]++;
+
+	for (i = 1; i <= (size_t)max; i++)
+		count[i] += count[i - 1];
+}
+
+/**
+ * counting_sort - Sorts an array using Counting sort
  * @array: Array to sort
  * @size: Size of the array
  */
@@ -15,11 +56,7 @@ void counting_sort(int *array, size_t size)
 	if (array == NULL || size < 2)
 		return;
 
-	max = array[0];
-	for (i = 1; i < size; i++)
-		if (array[i] > max)
-			max = array[i];
-
+	max = get_max(array, size);
 	count = malloc(sizeof(int) * (max + 1));
 	sorted = malloc(sizeof(int) * size);
 	if (count == NULL || sorted == NULL)
@@ -29,15 +66,7 @@ void counting_sort(int *array, size_t size)
 		return;
 	}
 
-	for (i = 0; i <= (size_t)max; i++)
-		count[i] = 0;
-
-	for (i = 0; i < size; i++)
-		count[array[i]]++;
-
-	for (i = 1; i <= (size_t)max; i++)
-		count[i] += count[i - 1];
-
+	build_count(array, size, count, max);
 	print_array(count, max + 1);
 
 	for (i = size; i > 0; i--)
