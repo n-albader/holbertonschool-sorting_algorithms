763989e4504493b7570aa53992e48c0e7bf85101
Lujain Alsultan <lujainabdulmohsen@hotmail.com>
2026-08-20 18:41:38 +0300

    Fix Betty style for Cocktail shaker sort

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
- * swap_nodes - Swaps two adjacent nodes in a doubly linked list
+ * swap_nodes - Swaps two adjacent nodes
  * @list: Pointer to the head of the list
  * @left: Left node
  * @right: Right node
@@ -9,12 +9,10 @@
 static void swap_nodes(listint_t **list, listint_t *left, listint_t *right)
 {
 	left->next = right->next;
-
 	if (right->next != NULL)
 		right->next->prev = left;
 
 	right->prev = left->prev;
-
 	if (left->prev != NULL)
 		left->prev->next = right;
 	else
@@ -25,53 +23,73 @@ static void swap_nodes(listint_t **list, listint_t *left, listint_t *right)
 }
 
 /**
- * cocktail_sort_list - Sorts a doubly linked list using Cocktail shaker sort
+ * forward_pass - Performs a forward pass
+ * @list: Pointer to the head of the list
+ * @end: End boundary
+ * @swapped: Swap flag
+ *
+ * Return: Last node reached
+ */
+static listint_t *forward_pass(listint_t **list, listint_t *end, int *swapped)
+{
+	listint_t *current = *list;
+
+	while (current->next != end)
+	{
+		if (current->n > current->next->n)
+		{
+			swap_nodes(list, current, current->next);
+			print_list(*list);
+			*swapped = 1;
+		}
+		else
+			current = current->next;
+	}
+	return (current);
+}
+
+/**
+ * backward_pass - Performs a backward pass
+ * @list: Pointer to the head of the list
+ * @current: Node to start from
+ * @swapped: Swap flag
+ */
+static void backward_pass(listint_t **list, listint_t *current, int *swapped)
+{
+	while (current->prev != NULL)
+	{
+		if (current->prev->n > current->n)
+		{
+			swap_nodes(list, current->prev, current);
+			print_list(*list);
+			*swapped = 1;
+		}
+		else
+			current = current->prev;
+	}
+}
+
+/**
+ * cocktail_sort_list - Sorts a list using Cocktail shaker sort
  * @list: Pointer to the head of the list
  */
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *current, *end;
-	int swapped;
+	listint_t *current, *end = NULL;
+	int swapped = 1;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	end = NULL;
-	swapped = 1;
-
 	while (swapped)
 	{
 		swapped = 0;
-		current = *list;
-
-		while (current->next != end)
-		{
-			if (current->n > current->next->n)
-			{
-				swap_nodes(list, current, current->next);
-				print_list(*list);
-				swapped = 1;
-			}
-			else
-				current = current->next;
-		}
-
+		current = forward_pass(list, end, &swapped);
 		if (swapped == 0)
 			break;
 
 		end = current;
 		swapped = 0;
-
-		while (current->prev != NULL)
-		{
-			if (current->prev->n > current->n)
-			{
-				swap_nodes(list, current->prev, current);
-				print_list(*list);
-				swapped = 1;
-			}
-			else
-				current = current->prev;
-		}
+		backward_pass(list, current, &swapped);
 	}
 }
